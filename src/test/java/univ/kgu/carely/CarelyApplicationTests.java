e33ff9aedaf9974d687c5612423f6e75b43b96f3
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-15 11:15:50 +0900

    Init

---
+++ src/test/java/univ/kgu/carely/CarelyApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.kgu.carely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
