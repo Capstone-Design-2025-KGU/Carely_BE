7ecf25d3a0bc59660bdf774c62ccafe80add9bc9
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-15 16:04:42 +0900

    feat : Address 에 Builder 패턴 적용 및 getter, setter, 생성자 관련 설정 추가

--- src/main/java/univ/kgu/carely/domain/common/embeded/Address.java
+++ src/main/java/univ/kgu/carely/domain/common/embeded/Address.java
@@ -3,8 +3,18 @@ package univ.kgu.carely.domain.common.embeded;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Address {
 
     private String province;
@@ -13,9 +23,9 @@ public class Address {
 
     private String district;
 
-    @Column(precision = 10, scale = 7, nullable = false)
+    @Column(name = "lat", precision = 10, scale = 7, nullable = false)
     private BigDecimal latitude;
 
-    @Column(precision = 10, scale = 7, nullable = false)
+    @Column(name = "lng", precision = 10, scale = 7, nullable = false)
     private BigDecimal longitude;
 }
