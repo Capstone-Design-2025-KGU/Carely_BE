7432d3a77bb9d76392f02e6736b7251ca10ba29b
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-15 16:03:26 +0900

    feat : MemberRepository 추가 및 설정

---
+++ src/main/java/univ/kgu/carely/domain/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package univ.kgu.carely.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import univ.kgu.carely.domain.member.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
