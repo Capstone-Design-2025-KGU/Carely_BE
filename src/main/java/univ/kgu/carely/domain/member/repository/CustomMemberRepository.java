96bf3cad38075e48c543b76d03eed90bd912e063
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-16 15:01:00 +0900

    feat : 위도/경도 기준 특정 범위 내의 유저 검색 기능 추가

--- src/main/java/univ/kgu/carely/domain/member/repository/CustomMemberRepository.java
+++ src/main/java/univ/kgu/carely/domain/member/repository/CustomMemberRepository.java
@@ -1,7 +1,11 @@
 package univ.kgu.carely.domain.member.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import univ.kgu.carely.domain.member.entity.Member;
 
 public interface CustomMemberRepository {
     Member findByName(String name);
+
+    List<Member> findAllWithin(BigDecimal lat, BigDecimal lng, int meter);
 }
--- src/main/java/univ/kgu/carely/domain/member/repository/MemberRepositoryImpl.java
+++ src/main/java/univ/kgu/carely/domain/member/repository/MemberRepositoryImpl.java
@@ -1,6 +1,11 @@
 package univ.kgu.carely.domain.member.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.math.BigDecimal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import univ.kgu.carely.domain.member.entity.Member;
 import univ.kgu.carely.domain.member.entity.QMember;
@@ -13,11 +18,31 @@ public class MemberRepositoryImpl implements CustomMemberRepository {
     private static final QMember member = QMember.member;
 
     @Override
-    public Member findByName(String name){
+    public Member findByName(String name) {
 
         return jpaQueryFactory.select(member)
                 .from(member)
                 .where(member.name.eq(name))
                 .fetchOne();
+    }
+
+    /**
+     * 입력한 위도/경도를 기준으로 해당 범위 내에 존재하는 모든 멤버를 찾는다.
+     * @param lat   중심이 되는 위치의 위도
+     * @param lng   중심이 되는 위치의 경도
+     * @param meter 중심으로부터 확인하려고 하는 범위
+     * @return 범위 내의 모든 멤버
+     */
+    @Override
+    public List<Member> findAllWithin(BigDecimal lat, BigDecimal lng, int meter) {
+        // MySQL에서 지원하는 위도/경도로 거리 비교하는 함수 이용
+        NumberExpression<Double> distance = Expressions.numberTemplate(Double.class,
+                        "ST_Distance_Sphere(POINT({0}, {1}), POINT({2}, {3}))",
+                lng, lat, member.address.longitude, member.address.latitude);
+
+        return jpaQueryFactory.select(member)
+                .from(member)
+                .where(distance.loe(meter))
+                .fetch();
     }
 }
