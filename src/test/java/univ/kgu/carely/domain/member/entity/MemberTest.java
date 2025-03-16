c16fd03c91f758d018fb88278691da84ab800e03
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-16 20:05:57 +0900

    test : 뷰포트 범위 내에서 찾고자 하는 이웃 검색 테스트로 변경

--- src/test/java/univ/kgu/carely/domain/member/entity/MemberTest.java
+++ src/test/java/univ/kgu/carely/domain/member/entity/MemberTest.java
@@ -2,7 +2,7 @@ package univ.kgu.carely.domain.member.entity;
 
 // import static org.junit.jupiter.api.Assertions.*;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import jakarta.transaction.Transactional;
 import java.math.BigDecimal;
@@ -12,6 +12,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import univ.kgu.carely.domain.common.enums.MemberType;
+import univ.kgu.carely.domain.map.dto.request.ReqViewPortInfoDTO;
+import univ.kgu.carely.domain.member.dto.response.ResMemberPublicInfoDTO;
 import univ.kgu.carely.domain.member.repository.MemberRepository;
 
 @Transactional
@@ -27,16 +30,21 @@ class MemberTest {
     void test1(){
         Member member = memberRepository.findByName("chogunhee");
 
-        assertThat(member.getId()).isEqualTo(1);
+        assertThat(member.getMemberId()).isEqualTo(1);
     }
 
     @Test
     @DisplayName("범위 내에 해당하는 유저 검색")
     void test2(){
+        ReqViewPortInfoDTO viewPortInfoDTO = new ReqViewPortInfoDTO();
+        viewPortInfoDTO.setLbLat(BigDecimal.valueOf(37.2869235));
+        viewPortInfoDTO.setLbLng(BigDecimal.valueOf(127.0190130));
+        viewPortInfoDTO.setRtLat(BigDecimal.valueOf(37.3119958));
+        viewPortInfoDTO.setRtLng(BigDecimal.valueOf(127.0578250));
         // 경기대 좌표
-        List<Member> allWithin = memberRepository.findAllWithinDistance(BigDecimal.valueOf(37.301387),
-                BigDecimal.valueOf(127.036554), 100);
+        List<ResMemberPublicInfoDTO> allWithin = memberRepository.findAllWithinDistance(BigDecimal.valueOf(37.301387),
+                BigDecimal.valueOf(127.036554), 2000, viewPortInfoDTO, MemberType.FAMILY);
 
-        assertThat(allWithin.get(0).getAddress().getDetails()).isEqualTo("경기대 도서관");
+        assertThat(allWithin).hasSize(5);
     }
 }
