30533ce0c06dce21a88e82f5d361adb3e42721ab
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-19 10:27:32 +0900

    comment : 주석 위치 수정

--- src/main/java/univ/kgu/carely/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/univ/kgu/carely/domain/member/service/impl/MemberServiceImpl.java
@@ -16,13 +16,6 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-    /**
-     * 인증된 동네 기준 주변 이웃을 검색한다.
-     * @param memberId    본인 아이디
-     * @param viewPortDTO 현재 뷰포트 정보
-     * @param memberType  검색하려고 하는 멤버 타입
-     * @return 검색된 데이터
-     */
     @Override
     public List<ResMemberPublicInfoDTO> searchNeighborMember(Long memberId, ReqViewPortInfoDTO viewPortDTO,
                                                              MemberType memberType) {
