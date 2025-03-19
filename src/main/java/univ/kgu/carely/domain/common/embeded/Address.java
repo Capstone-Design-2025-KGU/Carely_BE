82161808c68abb892c5a9f0d7328cb7608cbb0db
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-19 11:54:03 +0900

    comment : Member 엔티티의 요청 응답 관련 DTO들 Swagger 문서 작성

--- src/main/java/univ/kgu/carely/domain/common/embeded/Address.java
+++ src/main/java/univ/kgu/carely/domain/common/embeded/Address.java
@@ -19,18 +19,18 @@ import lombok.Setter;
 @Schema(title = "주소", description = "주소 정보")
 public class Address {
 
-    @Schema(description = "도")
+    @Schema(description = "도", example = "경기도")
     private String province;
-    @Schema(description = "시")
+    @Schema(description = "시", example = "수원시")
     private String city;
-    @Schema(description = "구")
+    @Schema(description = "구", example = "영통구")
     private String district;
-    @Schema(description = "세부 주소")
+    @Schema(description = "세부 주소", example = "경기대학교 도서관")
     private String details;
-    @Schema(description = "위도")
+    @Schema(description = "위도", example = "37.3011651")
     @Column(name = "lat", precision = 10, scale = 7, nullable = false)
     private BigDecimal latitude;
-    @Schema(description = "경도")
+    @Schema(description = "경도", example = "127.0356645")
     @Column(name = "lng", precision = 10, scale = 7, nullable = false)
     private BigDecimal longitude;
 }
--- src/main/java/univ/kgu/carely/domain/member/dto/response/ResMemberPrivateInfoDTO.java
+++ src/main/java/univ/kgu/carely/domain/member/dto/response/ResMemberPrivateInfoDTO.java
@@ -1,5 +1,6 @@
 package univ.kgu.carely.domain.member.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -16,20 +17,35 @@ import univ.kgu.carely.domain.common.enums.MemberType;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(title = "회원 개인 정보 응답 DTO", description = "회원 개인 정보 응답 DTO")
 public class ResMemberPrivateInfoDTO {
 
+    @Schema(description = "고유 멤버 ID")
     private Long memberId;
+    @Schema(description = "회원 ID")
     private String username;
+    @Schema(description = "회원 비밀번호")
     private String password;
+    @Schema(description = "회원 이름")
     private String name;
+    @Schema(description = "회원 전화번호")
     private String phoneNumber;
+    @Schema(description = "회원 생일")
     private LocalDate birth;
+    @Schema(description = "회원의 소개 문구")
     private String story;
+    @Schema(description = "회원 타입")
     private MemberType memberType;
+    @Schema(description = "회원의 공개 여부")
     private Boolean isVisible;
+    @Schema(description = "회원의 동네 인증 여부")
     private Boolean isVerified;
+    @Schema(description = "회원의 프로필 사진 경로")
     private String profileImage;
+    @Schema(description = "회원 생성 일시")
     private LocalDateTime createdAt;
+    @Schema(description = "회원 주소")
     private Address address;
+    @Schema(description = "회원 보조 기술")
     private Skill skill;
 }
