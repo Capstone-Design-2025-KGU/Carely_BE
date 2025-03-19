7adb1fa102cb7ac797d1591bb3564f5730176ea2
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-19 16:21:42 +0900

    comment : MemberRepository 주석 추가

--- src/main/java/univ/kgu/carely/domain/member/repository/MemberRepository.java
+++ src/main/java/univ/kgu/carely/domain/member/repository/MemberRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import univ.kgu.carely.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
+    /** 해당 username이 존재하는지 확인한다. */
     Boolean existsByUsername(String username);
+    /** 해당 phoneNumber가 존재하는지 확인한다. */
     Boolean existsByPhoneNumber(String phoneNumber);
 }
