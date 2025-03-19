7cfd9fc9250b3f0fd425e95e2045cb5618bde7ec
boroboro01 <duke7201@naver.com>
2025-03-19 13:40:19 +0900

    feat: overide mathod to web-socket config

--- src/main/java/univ/kgu/carely/config/WebSocketConfig.java
+++ src/main/java/univ/kgu/carely/config/WebSocketConfig.java
@@ -11,13 +11,23 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * 새로운 Stomp Endpoints를 추가하는 메서드입니다.
+     * @param registry
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
+        registry.addEndpoint("/ws").withSockJS();
     }
 
+    /**
+     * Application Destination Prefix를 설정합니다.
+     * Simple Broker를 언제 활성화 할지 결정합니다.
+     * @param registry
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
     }
 }
