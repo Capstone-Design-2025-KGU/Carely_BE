31e421863472c695b410d9b949fb2289a894a774
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-22 14:29:48 +0900

    feat : 능력 수정 API 추가 및 이웃 탐색 범위 상수화

--- src/main/java/univ/kgu/carely/domain/member/service/MemberService.java
+++ src/main/java/univ/kgu/carely/domain/member/service/MemberService.java
@@ -3,6 +3,7 @@ package univ.kgu.carely.domain.member.service;
 import java.util.List;
 import univ.kgu.carely.domain.map.dto.request.ReqCoordinationDTO;
 import univ.kgu.carely.domain.member.dto.request.ReqMemberCreateDTO;
+import univ.kgu.carely.domain.member.dto.request.ReqUpdateSkillDTO;
 import univ.kgu.carely.domain.member.dto.response.ResMemberPrivateInfoDTO;
 import univ.kgu.carely.domain.member.dto.response.ResMemberPublicInfoDTO;
 import univ.kgu.carely.domain.member.entity.Member;
@@ -56,4 +57,6 @@ public interface MemberService {
      * @return 개인 정보
      */
     ResMemberPrivateInfoDTO getPrivateInfo();
+
+    Boolean updateSkill(ReqUpdateSkillDTO reqUpdateSkillDTO);
 }
