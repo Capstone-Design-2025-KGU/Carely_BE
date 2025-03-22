8bf9716fcec829ee51647ed7e13a55442139b5e4
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-22 16:25:31 +0900

    comment : 팀 생성 DTO의 Swagger 문서 작성

--- src/main/java/univ/kgu/carely/domain/team/dto/request/ReqCreateTeamDTO.java
+++ src/main/java/univ/kgu/carely/domain/team/dto/request/ReqCreateTeamDTO.java
@@ -1,5 +1,6 @@
 package univ.kgu.carely.domain.team.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +9,12 @@ import univ.kgu.carely.domain.common.embeded.Address;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(title = "그룹 생성 요청 DTO", description = "그룹 생성할 때 필요한 정보를 요청하는 DTO")
 public class ReqCreateTeamDTO {
 
+    @Schema(description = "그룹의 이름", example = "경기대 학생들 모임")
     private String teamName;
+    @Schema(description = "그룹의 중심 주소", example = "그룹 주소")
     private Address address;
 
 }
