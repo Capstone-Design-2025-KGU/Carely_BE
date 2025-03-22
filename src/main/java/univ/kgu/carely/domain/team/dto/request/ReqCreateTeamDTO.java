d00728cc93b67830e6b95a27b06a7af266f4a0d7
whrjsgml0000 <whrjsgml4515@naver.com>
2025-03-22 15:18:33 +0900

    feat : 팀 생성 API 추가 및 로직 작성

--- src/main/java/univ/kgu/carely/domain/team/dto/request/ReqCreateTeamDTO.java
+++ src/main/java/univ/kgu/carely/domain/team/dto/request/ReqCreateTeamDTO.java
@@ -1,4 +1,16 @@
-package univ.kgu.carely.domain.team.dto;
+package univ.kgu.carely.domain.team.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import univ.kgu.carely.domain.common.embeded.Address;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class ReqCreateTeamDTO {
+
+    private String teamName;
+    private Address address;
+
 }
--- src/main/java/univ/kgu/carely/domain/team/service/impl/TeamServiceImpl.java
+++ src/main/java/univ/kgu/carely/domain/team/service/impl/TeamServiceImpl.java
@@ -1,4 +1,45 @@
 package univ.kgu.carely.domain.team.service.impl;
 
-public class TeamServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import univ.kgu.carely.domain.member.entity.Member;
+import univ.kgu.carely.domain.member.service.MemberService;
+import univ.kgu.carely.domain.team.dto.request.ReqCreateTeamDTO;
+import univ.kgu.carely.domain.team.entity.Team;
+import univ.kgu.carely.domain.team.entity.TeamMate;
+import univ.kgu.carely.domain.team.entity.TeamRole;
+import univ.kgu.carely.domain.team.repository.TeamRepository;
+import univ.kgu.carely.domain.team.service.TeamService;
+
+@Service
+@RequiredArgsConstructor
+public class TeamServiceImpl implements TeamService {
+
+    private final MemberService memberService;
+    private final TeamRepository teamRepository;
+
+    @Override
+    @Transactional
+    public Boolean createTeam(ReqCreateTeamDTO reqCreateTeamDTO) {
+        Member member = memberService.currentMember();
+
+        Team team = Team.builder()
+                .teamName(reqCreateTeamDTO.getTeamName())
+                .address(reqCreateTeamDTO.getAddress())
+                .build();
+
+        teamRepository.save(team);
+
+        TeamMate teamMate = TeamMate.builder()
+                .memberId(member)
+                .teamId(team)
+                .role(TeamRole.LEADER)
+                .build();
+
+        team.getTeamMates().add(teamMate);
+
+        return true;
+    }
+
 }
