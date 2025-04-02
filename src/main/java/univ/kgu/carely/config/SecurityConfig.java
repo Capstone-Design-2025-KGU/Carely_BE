301049ed1555f03194a5790bfd0f011e5e368efd
whrjsgml0000 <whrjsgml4515@naver.com>
2025-04-02 12:55:23 +0900

    feat : spring web mvc, spring security에 cors 설정 추가

--- src/main/java/univ/kgu/carely/config/SecurityConfig.java
+++ src/main/java/univ/kgu/carely/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package univ.kgu.carely.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import univ.kgu.carely.jwt.JwtFilter;
 import univ.kgu.carely.jwt.JwtUtil;
 import univ.kgu.carely.jwt.LoginFilter;
@@ -40,6 +46,20 @@ public class SecurityConfig {
         http.formLogin(auth->auth.disable());
 
         http.httpBasic(auth->auth.disable());
+
+        http.cors(custom -> custom.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+
+                config.setAllowedOrigins(Collections.singletonList("https://carely.my"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setMaxAge(3600L);
+                return config;
+            }
+        }));
 
         http
                 .authorizeHttpRequests(auth->auth
