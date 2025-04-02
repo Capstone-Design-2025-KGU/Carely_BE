378710e9dc384d67dc9c01444457ef2f39a97edf
boroboro01 <duke7201@naver.com>
2025-04-02 15:25:14 +0900

    fix: swagger 403 error

--- src/main/java/univ/kgu/carely/CarelyApplication.java
+++ src/main/java/univ/kgu/carely/CarelyApplication.java
@@ -2,10 +2,17 @@ package univ.kgu.carely;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server url")
+		}
+)
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO) // 페이징 관련 설정
 public class CarelyApplication {
