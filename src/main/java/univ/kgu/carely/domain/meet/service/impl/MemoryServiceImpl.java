d276b13b0ea607b6aa503df9fbf07c738fedb0bf
whrjsgml0000 <whrjsgml4515@naver.com>
2025-04-03 15:24:55 +0900

    comment : 논의가 필요한 내용 주석 추가

--- src/main/java/univ/kgu/carely/domain/meet/service/impl/MemoryServiceImpl.java
+++ src/main/java/univ/kgu/carely/domain/meet/service/impl/MemoryServiceImpl.java
@@ -7,4 +7,10 @@ import univ.kgu.carely.domain.meet.service.MemoryService;
 @Service
 @RequiredArgsConstructor
 public class MemoryServiceImpl implements MemoryService {
+
+    // Memory = 함께한 추억 = 방명록??
+    // 완료된 약속 하나 마다 방명록 작성이 가능한지
+    // 아니면 약속은 여러 개가 가능해도 방명록은 하나만 가능한지?
+    // 약속과 함께한 추억에 대한 연관관계 설정이 필요한 것 같음.
+
 }
