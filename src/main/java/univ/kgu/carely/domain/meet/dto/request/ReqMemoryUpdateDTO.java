6b5e109e8b8dd94b6ce89390ca8416d64e4a5501
whrjsgml0000 <whrjsgml4515@naver.com>
2025-04-05 12:14:38 +0900

    feat : Memory 엔티티 업데이트용 요청 DTO 추가

---
+++ src/main/java/univ/kgu/carely/domain/meet/dto/request/ReqMemoryUpdateDTO.java
@@ -0,0 +1,14 @@
+package univ.kgu.carely.domain.meet.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqMemoryUpdateDTO {
+
+    private String memory;
+
+}
