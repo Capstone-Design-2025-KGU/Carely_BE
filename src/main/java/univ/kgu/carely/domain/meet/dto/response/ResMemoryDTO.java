fb7610e21f5a5fa31335ba0c0e36654285c62369
whrjsgml0000 <whrjsgml4515@naver.com>
2025-04-05 12:13:40 +0900

    feat : Memory 엔티티 응답용 DTO 추가

---
+++ src/main/java/univ/kgu/carely/domain/meet/dto/response/ResMemoryDTO.java
@@ -0,0 +1,25 @@
+package univ.kgu.carely.domain.meet.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import univ.kgu.carely.domain.member.dto.response.ResMemberSmallInfoDTO;
+
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResMemoryDTO {
+
+    private Long memoryId;
+    private String senderMemo;
+    private String receiverMemo;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private ResMemberSmallInfoDTO sender;
+    private ResMemberSmallInfoDTO receiver;
+    private Long meetingId;
+
+}
