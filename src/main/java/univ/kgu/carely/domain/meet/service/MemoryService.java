0dcdb1878bfd2b63bcc1abe40313ee02f386083f
whrjsgml0000 <whrjsgml4515@naver.com>
2025-04-05 14:54:58 +0900

    comment : memoryService 주석 추가

--- src/main/java/univ/kgu/carely/domain/meet/service/MemoryService.java
+++ src/main/java/univ/kgu/carely/domain/meet/service/MemoryService.java
@@ -9,10 +9,30 @@ import univ.kgu.carely.domain.meet.entity.Memory;
 
 public interface MemoryService {
 
+    /**
+     * 방명록을 수정한다.
+     *
+     * @param memoryId           수정하려고 하는 방명록 ID
+     * @param reqMemoryUpdateDTO 수정하려고 하는 내용
+     * @return 수정된 방명록 정보
+     */
     ResMemoryDTO updateMemory(Long memoryId, ReqMemoryUpdateDTO reqMemoryUpdateDTO);
 
+    /**
+     * 방명록을 조회한다.
+     *
+     * @param memoryId 조회하려고 하는 방명록 ID
+     * @return 방명록 정보
+     */
     ResMemoryDTO readMemory(Long memoryId);
 
+    /**
+     * 본인과 쿼리 이름이 포함된 방명록을 찾는다.
+     *
+     * @param query    검색하려고 하는 사람 이름
+     * @param pageable 페이징 정보
+     * @return 해당하는 방명록 리스트
+     */
     Page<ResMemoryDTO> readPagedMemory(String query, Pageable pageable);
 
     ResMemoryDTO toResMemoryDTO(Memory memory);
