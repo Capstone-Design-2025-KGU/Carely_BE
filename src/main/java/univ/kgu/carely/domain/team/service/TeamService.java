174cad7d2e089e0ee5924d2f61d1aa35af958a9c
whrjsgml0000 <whrjsgml4515@naver.com>
2025-04-11 10:51:54 +0900

    refactor : TeamController에서 직접 서비스로 인증 정보 넘기도록 수정

--- src/main/java/univ/kgu/carely/domain/team/service/TeamService.java
+++ src/main/java/univ/kgu/carely/domain/team/service/TeamService.java
@@ -2,6 +2,7 @@ package univ.kgu.carely.domain.team.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import univ.kgu.carely.domain.member.entity.Member;
 import univ.kgu.carely.domain.team.dto.request.ReqCreateTeamDTO;
 import univ.kgu.carely.domain.team.dto.response.ResTeamOutlineDTO;
 
@@ -10,34 +11,38 @@ public interface TeamService {
     /**
      * 그룹 생성
      *
+     * @param member
      * @param reqCreateTeamDTO 그룹 생성에 필요한 정보를 담은 DTO
      * @return 그룹 생성 성공 여부
      */
-    Boolean createTeam(ReqCreateTeamDTO reqCreateTeamDTO);
+    Boolean createTeam(Member member, ReqCreateTeamDTO reqCreateTeamDTO);
 
     /**
      * 그룹 가입
      *
+     * @param member
      * @param teamId 가입하려고 하는 그룹의 id
      * @return 그룹 가입 성공 여부
      */
-    Boolean joinTeam(Long teamId);
+    Boolean joinTeam(Member member, Long teamId);
 
     /**
      * 그룹 탈퇴
      *
+     * @param member
      * @param teamId 탈퇴하려는 그룹의 id
      * @return 그룹 탈퇴 성공 여부
      */
-    Boolean exitTeam(Long teamId);
+    Boolean exitTeam(Member member, Long teamId);
 
     /**
      * 그룹 폐쇄
-     * 
+     *
+     * @param member
      * @param teamId 폐쇄하려는 그룹의 id
      * @return 그룹 폐쇄 성공 여부
      */
-    Boolean closeTeam(Long teamId);
+    Boolean closeTeam(Member member, Long teamId);
 
-    Page<ResTeamOutlineDTO> searchNeighbor(Pageable pageable);
+    Page<ResTeamOutlineDTO> searchNeighbor(Member member, Pageable pageable);
 }
