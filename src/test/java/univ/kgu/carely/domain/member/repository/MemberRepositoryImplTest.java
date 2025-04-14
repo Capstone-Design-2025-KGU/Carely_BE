5ef35d48fbbb49be699c9042b426f648a99846c1
whrjsgml0000 <whrjsgml4515@naver.com>
2025-04-14 19:30:46 +0900

    test : 추천 이웃 관련 쿼리 테스트 코드 작성

--- src/test/java/univ/kgu/carely/domain/member/repository/MemberRepositoryImplTest.java
+++ src/test/java/univ/kgu/carely/domain/member/repository/MemberRepositoryImplTest.java
@@ -12,7 +12,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
 import univ.kgu.carely.config.QslConfig;
+import univ.kgu.carely.domain.member.entity.Member;
 
 //@DataJpaTest
 @SpringBootTest
@@ -29,5 +31,14 @@ class MemberRepositoryImplTest {
 
         memberRepository.findAllWithinDistance("", gf.createPoint(new Coordinate(127.039357,37.300781)), 2000);
 
+    }
+
+    @Test
+    void findRecommendedMembers() {
+        Member member = memberRepository.findById(1L).orElseThrow();
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        memberRepository.findRecommendedMembers(2000, member, pageRequest);
     }
 }
