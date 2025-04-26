f2600e1119907068e0ff964285946ccffe1609c5
boroboro01 <duke7201@naver.com>
2025-04-26 12:01:06 +0900

    feat: extend message type for meeting

--- src/main/java/univ/kgu/carely/domain/chat/entity/MessageType.java
+++ src/main/java/univ/kgu/carely/domain/chat/entity/MessageType.java
@@ -2,12 +2,19 @@ package univ.kgu.carely.domain.chat.entity;
 
 public enum MessageType {
 
-    //유저가 메세지를 보냈음을 의미합니다.
+    // 유저가 일반 메세지를 보냈음을 의미합니다.
     CHAT,
 
-    //유저가 채팅에 접속함을 의미합니다.
+    // 유저가 채팅방에 입장했음을 의미합니다.
     JOIN,
 
-    //유저가 채팅을 나갔음을 의미합니다.
-    LEAVE
+    // 유저가 채팅방을 나갔음을 의미합니다.
+    LEAVE,
+
+    // 유저가 약속을 요청했음을 의미합니다.
+    MEETING_REQUEST,
+
+    // 유저가 약속을 수락했음을 의미합니다.
+    MEETING_ACCEPT
 }
+
