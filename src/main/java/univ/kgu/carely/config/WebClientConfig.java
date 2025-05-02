e5ea0d15ac5cc7b531a583b8338474e330fb2306
whrjsgml0000 <whrjsgml4515@naver.com>
2025-05-02 11:01:47 +0900

    feat : WebClientConfig 추가

---
+++ src/main/java/univ/kgu/carely/config/WebClientConfig.java
@@ -0,0 +1,22 @@
+package univ.kgu.carely.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("https://localhost:6789/api/summarization")
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                })
+                .build();
+    }
+
+}
