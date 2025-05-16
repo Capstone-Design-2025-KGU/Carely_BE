fa04608c5414919169739d4030fbe8798cf283bf
whrjsgml0000 <whrjsgml4515@naver.com>
2025-05-16 11:18:54 +0900

    feat : 로그인한 유저의 멤버 타입에 따라 다른 쿼리가 나가도록 수정

--- src/main/java/univ/kgu/carely/domain/meet/repository/meeting/impl/MeetingRepositoryImpl.java
+++ src/main/java/univ/kgu/carely/domain/meet/repository/meeting/impl/MeetingRepositoryImpl.java
@@ -7,12 +7,14 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import univ.kgu.carely.domain.common.enums.MemberType;
 import univ.kgu.carely.domain.meet.dto.response.ResMeetingSmallInfoDTO;
 import univ.kgu.carely.domain.meet.entity.MeetingStatus;
 import univ.kgu.carely.domain.meet.entity.QMeeting;
 import univ.kgu.carely.domain.meet.repository.meeting.CustomMeetingRepository;
 import univ.kgu.carely.domain.member.dto.response.ResMemberSmallInfoDTO;
 import univ.kgu.carely.domain.member.entity.Member;
+import univ.kgu.carely.domain.member.entity.QMember;
 
 @RequiredArgsConstructor
 public class MeetingRepositoryImpl implements CustomMeetingRepository {
@@ -33,25 +35,57 @@ public class MeetingRepositoryImpl implements CustomMeetingRepository {
 
     @Override
     public ResMeetingSmallInfoDTO findNearestMeetingBySenderOrReceiver(Member self) {
+        MemberType memberType = jpaQueryFactory.select(member.memberType)
+                .from(member)
+                .where(member.eq(self))
+                .fetchOne();
+
+        if (memberType.equals(MemberType.FAMILY)){
+            QMember receiver = meeting.receiver;
+
+            return jpaQueryFactory.select(Projections.fields(ResMeetingSmallInfoDTO.class,
+                            meeting.id.as("meetingId"),
+                            Projections.fields(ResMemberSmallInfoDTO.class,
+                                    receiver.memberId,
+                                    receiver.username,
+                                    receiver.name,
+                                    receiver.memberType,
+                                    receiver.profileImage).as("member"),
+                            meeting.startTime,
+                            memo.id.as("memoId"),
+                            memo.walkSum.as("walk"),
+                            memo.bathSum.as("bath"),
+                            memo.commSum.as("comm"),
+                            memo.mealSum.as("meal"),
+                            memo.toiletSum.as("toilet")))
+                    .from(meeting)
+                    .leftJoin(meeting.memos, memo)
+                    .where(receiver.eq(self)
+                            .and(meeting.status.eq(MeetingStatus.ACCEPT))
+                            .and(meeting.startTime.before(LocalDateTime.now())))
+                    .orderBy(meeting.startTime.asc())
+                    .fetchFirst();
+        }
+        QMember sender = meeting.sender;
+
         return jpaQueryFactory.select(Projections.fields(ResMeetingSmallInfoDTO.class,
                         meeting.id.as("meetingId"),
                         Projections.fields(ResMemberSmallInfoDTO.class,
-                                member.memberId,
-                                member.username,
-                                member.name,
-                                member.memberType,
-                                member.profileImage),
+                                sender.memberId,
+                                sender.username,
+                                sender.name,
+                                sender.memberType,
+                                sender.profileImage).as("member"),
                         meeting.startTime,
                         memo.id.as("memoId"),
-                        memo.walkSum,
-                        memo.bathSum,
-                        memo.commSum,
-                        memo.mealSum,
-                        memo.toiletSum))
+                        memo.walkSum.as("walk"),
+                        memo.bathSum.as("bath"),
+                        memo.commSum.as("comm"),
+                        memo.mealSum.as("meal"),
+                        memo.toiletSum.as("toilet")))
                 .from(meeting)
                 .leftJoin(meeting.memos, memo)
-                .where((meeting.sender.eq(self)
-                        .or(meeting.receiver.eq(self)))
+                .where(sender.eq(self)
                         .and(meeting.status.eq(MeetingStatus.ACCEPT))
                         .and(meeting.startTime.before(LocalDateTime.now())))
                 .orderBy(meeting.startTime.asc())
