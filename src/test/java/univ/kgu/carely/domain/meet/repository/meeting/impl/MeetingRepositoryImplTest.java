bda24a21320d8c85d52a7f21eb3a4c28ea98d250
whrjsgml0000 <whrjsgml4515@naver.com>
2025-05-18 13:23:04 +0900

    test : 가장 가까운 시일의 승락된 약속을 받는 쿼리 테스트

--- src/test/java/univ/kgu/carely/domain/meet/repository/meeting/impl/MeetingRepositoryImplTest.java
+++ src/test/java/univ/kgu/carely/domain/meet/repository/meeting/impl/MeetingRepositoryImplTest.java
@@ -1,11 +1,12 @@
 package univ.kgu.carely.domain.meet.repository.meeting.impl;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import univ.kgu.carely.domain.meet.dto.response.ResMeetingSmallInfoDTO;
 import univ.kgu.carely.domain.meet.repository.meeting.MeetingRepository;
 import univ.kgu.carely.domain.member.repository.MemberRepository;
 
@@ -18,7 +19,19 @@ class MeetingRepositoryImplTest {
     MemberRepository memberRepository;
 
     @Test
-    void findNearestMeetingBySenderOrReceiver() {
-        meetingRepository.findNearestMeetingBySenderOrReceiver(memberRepository.getReferenceById(1L));
+    void findNearestMeetingByReceiver() {
+        ResMeetingSmallInfoDTO nearestMeetingBySenderOrReceiver = meetingRepository.findNearestMeetingBySenderOrReceiver(
+                memberRepository.getReferenceById(1L));
+
+        assertThat(nearestMeetingBySenderOrReceiver.getMeetingId()).isEqualTo(6);
     }
+
+    @Test
+    void findNearestMeetingBySender() {
+        ResMeetingSmallInfoDTO nearestMeetingBySenderOrReceiver = meetingRepository.findNearestMeetingBySenderOrReceiver(
+                memberRepository.getReferenceById(3L));
+
+        assertThat(nearestMeetingBySenderOrReceiver.getMeetingId()).isEqualTo(5);
+    }
+
 }
