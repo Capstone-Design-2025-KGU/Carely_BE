15b1dddebd9137171363462fed93d6ad05813a06
whrjsgml0000 <whrjsgml4515@naver.com>
2025-05-22 14:08:05 +0900

    test : 함께한 추억 리스트 반환 쿼리 테스트 코드 작성

--- src/test/java/univ/kgu/carely/domain/meet/repository/memory/MemoryRepositoryTest.java
+++ src/test/java/univ/kgu/carely/domain/meet/repository/memory/MemoryRepositoryTest.java
@@ -1,4 +1,31 @@
+package univ.kgu.carely.domain.meet.repository.memory;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import univ.kgu.carely.domain.meet.dto.response.ResMemoryCardDTO;
+import univ.kgu.carely.domain.member.entity.Member;
+import univ.kgu.carely.domain.member.repository.MemberRepository;
+
+@SpringBootTest
 class MemoryRepositoryTest {
-  
+
+    @Autowired
+    MemoryRepository memoryRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void test1() {
+        Member flutter = memberRepository.getReferenceById(1L);
+
+        List<ResMemoryCardDTO> cardByMember = memoryRepository.findMemoryCardByMember(5, flutter);
+
+
+    }
+
 }
