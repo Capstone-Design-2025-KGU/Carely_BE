e452443183320c60eb1351a6e9a031eebdeb6ea3
whrjsgml0000 <whrjsgml4515@naver.com>
2025-05-22 14:02:39 +0900

    feat : MemoryCard 반환 쿼리 작성

--- src/main/java/univ/kgu/carely/domain/meet/repository/memory/CustomMemoryRepository.java
+++ src/main/java/univ/kgu/carely/domain/meet/repository/memory/CustomMemoryRepository.java
@@ -1,10 +1,14 @@
 package univ.kgu.carely.domain.meet.repository.memory;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import univ.kgu.carely.domain.meet.dto.response.ResMemoryCardDTO;
 import univ.kgu.carely.domain.meet.dto.response.ResMemoryDTO;
 import univ.kgu.carely.domain.member.entity.Member;
 
 public interface CustomMemoryRepository {
     Page<ResMemoryDTO> findPagedMemoryByNameContaining(String query, Member me, Pageable pageable);
+
+    List<ResMemoryCardDTO> findMemoryCardByMember(int count, Member target);
 }
