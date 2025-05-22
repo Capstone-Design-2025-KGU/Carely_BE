c808e2db6a5147a9f583ea892413ab03ab98114b
whrjsgml0000 <whrjsgml4515@naver.com>
2025-05-22 15:34:06 +0900

    test : MemoryCard 반환 테스트 코드 작성

--- src/test/java/univ/kgu/carely/domain/meet/repository/memory/MemoryRepositoryTest.java
+++ src/test/java/univ/kgu/carely/domain/meet/repository/memory/MemoryRepositoryTest.java
@@ -1,8 +1,11 @@
 package univ.kgu.carely.domain.meet.repository.memory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,12 +23,28 @@ class MemoryRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
+    @DisplayName("View Of Receiver")
     void test1() {
         Member flutter = memberRepository.getReferenceById(1L);
 
         List<ResMemoryCardDTO> cardByMember = memoryRepository.findMemoryCardByMember(5, flutter);
 
+        assertEquals("ㅂㅈㄷㄱ", cardByMember.get(0).getMemo());
+        assertEquals("회원2", cardByMember.get(0).getOppoName());
+        assertEquals("ㅁㄴㅇㄹ", cardByMember.get(1).getMemo());
+        assertEquals("회원3", cardByMember.get(1).getOppoName());
+        assertThrows(IndexOutOfBoundsException.class, () -> cardByMember.get(3));
+    }
 
+    @Test
+    @DisplayName("View Of Sender")
+    void test2() {
+        Member member3 = memberRepository.getReferenceById(3L);
+
+        List<ResMemoryCardDTO> cardByMember = memoryRepository.findMemoryCardByMember(5, member3);
+
+        assertThat(cardByMember.get(0).getOppoName()).isEqualTo("박성민");
+        assertThat(cardByMember.get(0).getMemo()).contains("고생");
     }
 
 }
