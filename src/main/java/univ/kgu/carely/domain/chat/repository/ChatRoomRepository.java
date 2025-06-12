4474044bdb1f5bafd05ba6fdf52d6681243707ff
boroboro01 <duke7201@naver.com>
2025-06-12 14:26:24 +0900

    feat: update chat room repository for create chat room

--- src/main/java/univ/kgu/carely/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/univ/kgu/carely/domain/chat/repository/ChatRoomRepository.java
@@ -1,7 +1,18 @@
 package univ.kgu.carely.domain.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import univ.kgu.carely.domain.chat.entity.ChatRoom;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("""
+        SELECT r FROM ChatRoom r
+        JOIN ChatMember m1 ON m1.chatRoom = r AND m1.member.memberId = :member1
+        JOIN ChatMember m2 ON m2.chatRoom = r AND m2.member.memberId = :member2
+    """)
+    Optional<ChatRoom> findByMemberIds(@Param("member1") Long member1, @Param("member2") Long member2);
 }
